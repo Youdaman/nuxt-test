8f725f16e586b9facdb31106206b35e5fc7b676d
Stew Heckenberg <stewart.heckenberg@gmail.com>
2018-10-01 01:19:51 +1000

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+section.section
+  .columns.is-mobile
+    .column
+       logo
+    .column(v-for='(feature, i) of features', :key='i')
+      .card
+        header.card-header
+          p.card-header-title.has-text-grey
+            | {{ feature.title }}
+        .card-content
+          .content.has-text-centered
+            b-icon(:icon='feature.icon', size='is-large', type='is-primary')
+        footer.card-footer
+          .card-footer-item(v-html='feature.content')
+</template>
+
+<script lang="coffee">
+import Logo from '~/components/Logo.vue'
+
+export default {
+  name: 'HomePage'
+  components: { Logo }
+  data: ->
+    features: [
+      {
+        icon: 'github-circle'
+        title: 'Free'
+        content: '<span>Open source on <a href="https://github.com/buefy/buefy"> GitHub</a></span>'
+      }
+      {
+        icon: 'cellphone-link'
+        title: 'Responsive'
+        content: '<span><b class="has-text-grey">Every</b> component is responsive</span>'
+      }
+      {
+        icon: 'alert-decagram'
+        title: 'Modern'
+        content: '<span>Built with <a href="https://vuejs.org/">Vue.js</a> and <a href="http://bulma.io/">Bulma</a></span>'
+      }
+      {
+        icon: 'arrange-bring-to-front'
+        title: 'Lightweight'
+        content: '<span>No other internal dependency</span>'
+      }
+    ]
+}
+</script>
+
+<style lang="sass">
+.card-footer-item
+  color: green
+</style>
